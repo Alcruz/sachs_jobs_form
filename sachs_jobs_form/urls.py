1d7c012f043fcfab417db00ab1c020ebb7706861
Alvin Jonel <alvinj.delacruz@gmail.com>
2016-06-17 03:25:34 -0400

    Redirect home url to jobs form

--- sachs_jobs_form/urls.py
+++ sachs_jobs_form/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 
+from django.views.generic.base import RedirectView
+
 urlpatterns = [
+    url(r'^$', RedirectView.as_view(url='/jobs/apply')),
     url(r'^admin/', admin.site.urls),
     url(r'^jobs/', include('jobs_form.urls'))
 ]
